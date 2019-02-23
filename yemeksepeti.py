2decf92304db38a1f65ec1b42268c461b8c3bb89
Enis Getmez <mentalistler@mit.tc>
2019-02-23 22:46:20 +0300

    Update yemeksepeti.py

--- yemeksepeti.py
+++ yemeksepeti.py
@@ -10,19 +10,21 @@ from  openpyxl import *
 
 linktxt = open("linkler.txt" , "r")
 linkler = linktxt.readlines() 
-print(linkler)
-
 
 
 kitap = Workbook() # excell olustur
-kitap.create_sheet("veriler")
-yaz = kitap.get_sheet_by_name("veriler") 
-
+kitap.create_sheet("veriler") 
+yaz = kitap.get_sheet_by_name("veriler")
+yaz.append(['Bölge','Areaname','CatalogName' , 'CategoryName' , 'ClosedByParent' , 'CuisineNameList', 'DeliveryFee', 'DeliveryTime', 'DisplayName', 'Flavour', 'HasDVDPromotion', 'IsOpen', 'IsRestaurantOpen', 'MainCuisineId', 'MainCuisineLabelName', 'MainCuisineName', 'MinimumDeliveryPrice', 'OpenRestaurantCount', 'PaymentMethodsText', 'PaymentMethodsList', 'Serving', 'Slug', 'Speed', 'WorkHoursText', 'AvgPoint', 'ServingText', 'SpeedText', 'FlavourText', 'AvgRestaurantScore', 'MinimumDeliveryPriceText', 'HasCampusDiscount', 'IsFreezoneRestaurant', 'HasPromotion', 'SuperDelivery']) # sütun adı takım numarası
 
 for i in range(len(linkler)):
+
 	adres = linkler[i]
-	url = ("https://www.yemeksepeti.com/istanbul/"+adres)
-	sayfa = urllib.request.urlopen(url)
+	try:
+		url = ("https://www.yemeksepeti.com/"+adres)
+		sayfa = urllib.request.urlopen(url)
+	except:
+		pass
 	soup = BeautifulSoup(sayfa, "html.parser")
 
 	soupp = soup.encode("utf-8")
@@ -30,76 +32,44 @@ for i in range(len(linkler)):
 	for item in veri:
 		c = item['data-tooltip']
 		json = JSON.loads(c) 
-		Areaname = json['AreaName']
-		CatalogName = json['CatalogName']
-		CategoryName = json['CategoryName']
-		ClosedByParent = json['ClosedByParent']
-		CuisineNameList = json['CuisineNameList']
-		DeliveryFee = json['DeliveryFee']
+		Areaname = json["AreaName"].encode("utf-8")
+		CatalogName = json["CatalogName"]
+		CategoryName = json["CategoryName"]
+		ClosedByParent = json["ClosedByParent"]
+		CuisineNameList = json["CuisineNameList"]
+		DeliveryFee = json["DeliveryFee"]
 		DeliveryTime = json['DeliveryTime']
-		DisplayName = json['DisplayName']
-		Flavour = json['Flavour']
-		HasDVDPromotion = json['HasDVDPromotion']
-		ImagePath = json['ImagePath']
-		ImageFullPath = json['ImageFullPath']
-		ImageLabelListFullPath = json['ImageLabelListFullPath']
-		IsOpen = json['IsOpen']
-		IsRestaurantOpen = json['IsRestaurantOpen']
-		MainCuisineId = json['MainCuisineId']
-		MainCuisineLabelName = json['MainCuisineLabelName']
-		MainCuisineName = json['MainCuisineName']
-		MinimumDeliveryPrice = json['MinimumDeliveryPrice']
-		OpenRestaurantCount = json['OpenRestaurantCount']
-		PaymentMethodsText = json['PaymentMethodsText']
-		PaymentMethodsList = json['PaymentMethodsList']
-		#PromotionText = json['PromotionText']
-		Serving = json['Serving']
-		Slug = json['Slug']
-		Speed = json['Speed']
-		WorkHoursText = json['WorkHoursText']
-		AvgPoint = json['AvgPoint']
-		ServingText = json['ServingText']
-		SpeedText = json['SpeedText']
-		FlavourText = json['FlavourText']
-		AvgRestaurantScore = json['AvgRestaurantScore']
-		MinimumDeliveryPriceText = json['MinimumDeliveryPriceText']
-		HasCampusDiscount = json['HasCampusDiscount']
-		IsFreezoneRestaurant = json['IsFreezoneRestaurant']
-		HasPromotion = json['HasPromotion']
-		SuperDelivery = json['SuperDelivery']
-		print(Areaname)
-		print(CatalogName)
-		print(CategoryName)
-		print(ClosedByParent)
-		print(CuisineNameList)
-		print(DeliveryFee)
-		print(DeliveryTime)
-		print(DisplayName)
-		print(Flavour)
-		print(HasDVDPromotion)
-		print(ImagePath)
-		print(ImageFullPath)
-		print(ImageLabelListFullPath)
-		print(IsOpen)
-		print(IsRestaurantOpen)
-		print(MainCuisineId)
-		print(MainCuisineLabelName)
-		print(MainCuisineName)
-		print(MinimumDeliveryPrice)
-		print(OpenRestaurantCount)
-		print(PaymentMethodsText)
-		print(PaymentMethodsList)
-		print(Serving)
-		print(Slug)
-		print(Speed)
-		print(WorkHoursText)
-		print(AvgPoint)
-		print(ServingText)
-		print(SpeedText)
-		print(FlavourText)
-		print(AvgRestaurantScore)
-		print(MinimumDeliveryPriceText)
-		print(HasCampusDiscount)
-		print(IsFreezoneRestaurant)
-		print(HasPromotion)
-		print(SuperDelivery)
+		DisplayName = json['DisplayName'].encode("utf-8")
+		Flavour = json["Flavour"]
+		HasDVDPromotion = json["HasDVDPromotion"]
+		ImagePath = json["ImagePath"]
+		ImageFullPath = json["ImageFullPath"]
+		ImageLabelListFullPath = json["ImageLabelListFullPath"]
+		IsOpen = json["IsOpen"]
+		IsRestaurantOpen = json["IsRestaurantOpen"]
+		MainCuisineId = json["MainCuisineId"]
+		MainCuisineLabelName = json["MainCuisineLabelName"]
+		MainCuisineName = json["MainCuisineName"]
+		MinimumDeliveryPrice = json["MinimumDeliveryPrice"]
+		OpenRestaurantCount = json["OpenRestaurantCount"]
+		PaymentMethodsText = json["PaymentMethodsText"]
+		PaymentMethodsList = json["PaymentMethodsList"]
+		#PromotionText = json["PromotionText"]
+		Serving = json["Serving"]
+		Slug = json["Slug"]
+		Speed = json["Speed"]
+		WorkHoursText = json["WorkHoursText"]
+		AvgPoint = json["AvgPoint"]
+		ServingText = json["ServingText"]
+		SpeedText = json["SpeedText"]
+		FlavourText = json["FlavourText"]
+		AvgRestaurantScore = json["AvgRestaurantScore"]
+		MinimumDeliveryPriceText = json["MinimumDeliveryPriceText"]
+		HasCampusDiscount = json["HasCampusDiscount"]
+		IsFreezoneRestaurant = json["IsFreezoneRestaurant"]
+		HasPromotion = json["HasPromotion"]
+		SuperDelivery = json["SuperDelivery"]
+		yaz.append([adres,Areaname,CatalogName.replace("['" , "") , CategoryName, ClosedByParent ,str(CuisineNameList).strip('[]'), DeliveryFee, DeliveryTime, DisplayName, Flavour, HasDVDPromotion, IsOpen, IsRestaurantOpen, MainCuisineId, MainCuisineLabelName, MainCuisineName, MinimumDeliveryPrice,OpenRestaurantCount, str(PaymentMethodsText).strip('[]'), str(PaymentMethodsList).strip('[]'), Serving, Slug, Speed, WorkHoursText, AvgPoint, ServingText, SpeedText, FlavourText, AvgRestaurantScore, MinimumDeliveryPriceText, HasCampusDiscount, IsFreezoneRestaurant, HasPromotion, SuperDelivery]) # sütun adı takım numarası
+		kitap.save("yemeksepeti.xlsx") # exceli kaydet
+		print("1")
+kitap.close() #excelli kapat
